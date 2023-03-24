b619e81e31cd18624ff155fe0d8a3ef4d4adf4c9
Riyadh <riyadh.ju@gmail.com>
2023-03-24 23:36:36 +0600

    added logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css";
 import { Article, Brand, Feature, Navbar } from "./components";
 import {
   Footer,
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./navbar.css";
+import logo from "../../assets/GPT-3.svg";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3_navbar">
+      <div className="gpt3_navbar_links">
+        <div className="gpt3_navbar_links">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
