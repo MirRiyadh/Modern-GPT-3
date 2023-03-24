ace42b08d0d28da229bed78639b801bcd8d28967
Riyadh <riyadh.ju@gmail.com>
2023-03-24 18:06:45 +0600

    added homepage structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from "react";
 import { Article, Brand, Feature, Navbar } from "./components";
+import {
+  Footer,
+  Blog,
+  MultipileFeatures,
+  Possibility,
+  GPT3,
+  Header,
+} from "./containers";
 
 function App() {
   return (
     <div className="App">
-      <h1>GPT-3</h1>
-      <Article />
+      <div className="gradient_bg">
+        <Navbar />
+        <Header />
+      </div>
       <Brand />
-      <Feature />
-      <Navbar />
+      <GPT3 />
+      <MultipileFeatures />
+      <Possibility />
+      {/* <CTA/> */}
+      <Blog />
+      <Footer />
     </div>
   );
 }
---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export { default as Blog } from "./blog/Blog";
+export { default as Footer } from "./footer/Footer";
+export { default as GPT3 } from "./GPT3/GPT3";
+export { default as Header } from "./header/Header";
+export { default as MultipileFeatures } from "./multiple features/MultipileFeatures";
+export { default as Possibility } from "./possibility/Possibility";
