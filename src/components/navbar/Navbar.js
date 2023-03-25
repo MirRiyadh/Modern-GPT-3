0a0e73b1463c7d7f123bbb3628a216f15abf121a
Riyadh <riyadh.ju@gmail.com>
2023-03-25 10:44:38 +0600

    added navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,9 +7,38 @@ const Navbar = () => {
   return (
     <div className="gpt3_navbar">
       <div className="gpt3_navbar_links">
-        <div className="gpt3_navbar_links">
+        {/* logo */}
+        <div className="gpt3_navbar_links_logo">
           <img src={logo} alt="logo" />
         </div>
+
+        {/* menu links */}
+        <div className="gpt3_navbar_links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+
+          <p>
+            <a href="#gpt3">What is GPT3</a>
+          </p>
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#features">Features</a>
+          </p>
+          <p>
+            <a href="#Blog">Blog</a>
+          </p>
+          <p>
+            <a href="#about">About</a>
+          </p>
+        </div>
+      </div>
+      {/* gpt3_navbar_sign */}
+      <div className="gpt3_navbar_sign">
+        <p>Sign In</p>
+        <button type="button">Sign Up</button>
       </div>
     </div>
   );
