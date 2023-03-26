0bcc2da7d80bb8c6151e3cf822f0bec82d83839d
Riyadh <riyadh.ju@gmail.com>
2023-03-26 16:52:30 +0600

    added header content

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,8 +1,37 @@
 import React from "react";
 import "./Header.css";
+import people from "../../assets/Group 81.png";
+import ai from "../../assets/Illustration.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="gpt3_header section_padding" id="home">
+      <div className="gpt3_header_content">
+        <h1 className="gradient_text">
+          Let's Build Something amazing with GPT-3 OpenAl
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <div className="gpt3_header_content_input">
+          <input type="email" placeholder="your email address" />
+          <button type="button">Get started</button>
+        </div>
+
+        <div className="gpt3_header_content_people">
+          <img src={people} alt="peoples" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+
+        <div className="gpt3_header_image">
+          <img src={ai} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
