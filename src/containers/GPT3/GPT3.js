ec846db509df7582c2db5d4e62625b8694861c51
Riyadh <riyadh.ju@gmail.com>
2023-03-27 19:43:43 +0600

    added gpt3

--- src/containers/GPT3/GPT3.js
+++ src/containers/GPT3/GPT3.js
@@ -1,8 +1,38 @@
 import React from "react";
+import Feature from "../../components/feature/Feature";
 import "./GPT3.css";
 
 const GPT3 = () => {
-  return <div>GPT3</div>;
+  return (
+    <div className="gpt3_whatgpt3 section_margin" id="gpt3">
+      <div className="gpt3_whatgpt3_feature">
+        <Feature
+          title="What is GPT-3"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+        />
+      </div>
+      <div className="gpt3_whatgpt3_heading">
+        <h1 className="gradient_text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3_whatgpt3_container">
+        <Feature
+          title="ChatBots"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. "
+        />
+        <Feature
+          title="Knowledgebase"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b "
+        />
+        <Feature
+          title="Education"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments "
+        />
+      </div>
+    </div>
+  );
 };
 
 export default GPT3;
