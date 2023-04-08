20a128c009f160928438ef3515dfd25a12dc63c0
Riyadh <riyadh.ju@gmail.com>
2023-04-08 21:02:58 +0600

    added final footer style

--- src/containers/footer/Footer.js
+++ src/containers/footer/Footer.js
@@ -44,7 +44,7 @@ const Footer = () => {
       </div>
 
       <div className="gpt3_footer_copyright">
-        <p>@2022 GPT-3. All rights reserved.</p>
+        <p>@2023 GPT-3. All rights reserved.</p>
       </div>
     </div>
   );
